55ecc55eb8181d1c3eb726f740f32f98e2440f1b
Allan Cleysson <allancleysson@gmail.com>
2018-11-12 14:06:55 +0000

    resolução exercicio 9

---
+++ Exercicios/Exs_009.py
@@ -0,0 +1,10 @@
+# ==== Exercício 9: Exiba a tabuada de um numero escolhido
+
+# lendo numero 
+numero = int(input("Digite um numero para ver sua tabuada: "))
+
+# gerando tabuada dinamicamente com estrutura de repetição
+i = 1
+for i in range(1,11):
+    print("{} X {} = {}".format(numero, i, numero * i))
+    i = i + 1
