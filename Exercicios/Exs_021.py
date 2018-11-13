972adf09faa8bc6791824dff1460db5bb560f7de
Allan Cleysson <allancleysson@gmail.com>
2018-11-13 13:24:52 +0000

    correção de texto

--- Exercicios/Exs_021.py
+++ Exercicios/Exs_021.py
@@ -1,6 +1,6 @@
 # ==== Exercício 21: Abrir e Reproduzir um arquivo mp3
 '''
-Para esse exercício resolvi utilizar o módulo "Playsound" que faz o necessário de forma muito simples.
+Para esse exercício, resolvi utilizar o módulo "Playsound" que faz o necessário de forma muito simples.
 A documentação desse módulo encontra-se em: https://github.com/TaylorSMarks/playsound
 '''
 
