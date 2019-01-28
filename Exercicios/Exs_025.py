5e55a0b234c84ffe1ebdc2519949f6d2f8661837
Allan Cleysson <allancleysson@gmail.com>
2019-01-28 16:46:35 +0000

    alteração exs 25

--- Exercicios/Exs_025.py
+++ Exercicios/Exs_025.py
@@ -1,3 +1,3 @@
-# ==== Exercício 24: Identificar se um nome completo contem "silva"
+# ==== Exercício 25: Identificar se um nome completo contem "silva"
 splitNome = input('Digite seu nome completo: ').strip().upper().split()
 print('SILVA' in splitNome)
