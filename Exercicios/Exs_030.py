fe6c89e308aa524eb7c27bbed6d60450c64bb634
Allan Cleysson <allancleysson@gmail.com>
2019-04-05 14:00:24 +0000

    Exercicio 30

---
+++ Exercicios/Exs_030.py
@@ -0,0 +1,6 @@
+# ==== Exercício 30: descobrir se numero é par ou impar
+num = int(input('Digite um número: '))
+if ((num % 2) == 0):
+    print('O número {} é par'.format(num))
+else:
+    print('O número {} é impar'.format(num))
